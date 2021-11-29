8fb75af25530ee11193518abeb8e843ff48a1f73
Oseas <oarnaudsanchez23@gmail.com>
2021-11-28 22:22:25 -0400

    Project Files and Styles

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RickAndMortyApp } from './RickAndMortyApp';
+
+export const App = () => {
+    return (
+        <div>
+            <RickAndMortyApp />
+        </div>
+    )
+}
